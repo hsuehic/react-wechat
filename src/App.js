5d880230965482bae93c61c267e7fbecfd9b9eda
兴斋 <xingzhai.xxw@alibaba-inc.com>
2018-06-09 16:48:53 +0800

    feat: layout.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,46 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Badge, Icon, NavBar, Tabs } from 'antd-mobile';
 import './App.css';
+
+const tabs = [
+  { title: <Badge text={'3'}>微信</Badge> },
+  { title: <Badge text={'5'}>通信录</Badge> },
+  { title: <Badge dot>发现</Badge> },
+  { title: <Badge dot>我</Badge> },
+];
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="app">
+         <NavBar
+          className="header"
+          mode="dark"
+          leftContent={"微信"}
+          rightContent={[
+            <Icon key="0" type="search" style={{ marginRight: '16px' }} />,
+            <Icon key="1" type="ellipsis" />,
+          ]}
+        ></NavBar>
+        <Tabs tabs={tabs}
+          className="body"
+          initialPage={1}
+          tabBarPosition="bottom"
+          renderTab={tab => <span>{tab.title}</span>}
+        >
+          <div className="body-pane">
+            Content of first tab
+          </div>
+          <div className="body-pane">
+            Content of second tab
+          </div>
+          <div className="body-pane">
+            Content of third tab
+          </div>
+          <div className="body-pane">
+            Content of third tab
+          </div>
+        </Tabs>
       </div>
     );
   }
