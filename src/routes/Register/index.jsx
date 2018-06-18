454c051069186eae730168f465a15723e4916a78
兴斋 <xingzhai.xxw@alibaba-inc.com>
2018-06-18 21:14:25 +0800

    feat: register.

--- src/routes/Register/index.jsx
+++ src/routes/Register/index.jsx
@@ -6,16 +6,27 @@
  */
 
 import React from 'react'
-import { Button, Icon, List, InputItem, NavBar } from 'antd-mobile'
+import { Button, Icon, InputItem, List, NavBar, Toast } from 'antd-mobile'
 import ImagePicker from '../../components/ImagePicker'
+import { request } from '../../utils/fetch'
 
 import './index.less'
 
 export default class Component extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {}
+    this.state = {
+      nick: '',
+      thumb: '',
+      phone: '',
+      email: '',
+      region: '+86',
+      password: '',
+      cpassword: '',
+      useName: ''
+    }
     this.onGoBack = this.onGoBack.bind(this)
+    this.onRegister = this.onRegister.bind(this)
   }
 
   componentDidMount() {
@@ -26,7 +37,36 @@ export default class Component extends React.Component {
     history.goBack()
   }
 
+  onRegister() {
+    const { nick, thumb, userName, password, cpassword, region, email } = this.state
+    if (password !== cpassword) {
+      Toast.fail('两次输入密码不一致，请重新输入')
+    } else {
+      let { phone } = this.state
+      phone = phone.replace(/\s/mg, '')
+      const params = { nick, thumb, userName, password, region, email, phone }
+      const p = request('/api/reg', params)
+      p.then(res => {
+        if (res.code === 0) {
+          Toast.success('注册成功，请登录')
+          const { history } = this.props
+          history.push('/login')
+        } else {
+          Toast.fail(res.message)
+        }
+      })
+    }
+  }
+
   render() {
+    const stateChangeHandle = (stateName) => {
+      return (value) => {
+        this.setState({
+          [stateName]: value
+        })
+      }
+    }
+
     return <div className="app">
       <NavBar
         className="header"
@@ -34,18 +74,19 @@ export default class Component extends React.Component {
       />
       <div className="body">
         <List
-          renderFooter={<Button type="primary" className="wechat">注册</Button>}
+          renderFooter={<Button type="primary" className="wechat" onClick={this.onRegister}>注册</Button>}
         >
           <div styleName="nick">
             <div styleName="input">
               <InputItem
                 defaultValue=""
                 name="nick"
+                onChange={stateChangeHandle('nick')}
               >
                 昵称
               </InputItem>
             </div>
-            <ImagePicker styleName="image" />
+            <ImagePicker maxSize={50 * 1024} styleName="image" onChange={stateChangeHandle('thumb')} />
           </div>
           <List.Item
             extra={<span className="wechat-color">中国(+86)</span>}
@@ -55,20 +96,39 @@ export default class Component extends React.Component {
           <InputItem
             type="phone"
             name="phone"
+            onChange={stateChangeHandle('phone')}
           >
             手机号
           </InputItem>
           <InputItem
             type="text"
+            name="email"
+            onChange={stateChangeHandle('email')}
+          >
+            Email
+          </InputItem>
+          <InputItem
+            type="text"
             name="userName"
+            onChange={stateChangeHandle('userName')}
           >
             用户名
           </InputItem>
           <InputItem
             type="password"
             defaultValue=""
+            name="password"
+            onChange={stateChangeHandle('password')}
           >
             密码
+          </InputItem>
+          <InputItem
+            type="password"
+            name="cpassword"
+            defaultValue=""
+            onChange={stateChangeHandle('cpassword')}
+          >
+            确认密码
           </InputItem>
         </List>
       </div>
