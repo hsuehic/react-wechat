e8553ad6278f454efaca46201e58b6214d37e316
兴斋 <xingzhai.xxw@alibaba-inc.com>
2018-07-08 12:59:08 +0800

    style: line height.

---
+++ src/containers/App/route.jsx
@@ -0,0 +1,85 @@
+/**
+ * Copyright(c) xiaowei.hsueh@gmail.com.
+ *
+ * Authors: Richard Hsueh <xiaowei.hsueh@gmail.com> (https://www.gistop.com/)
+ * @Date   : 2018-7-8 12:41:08
+ */
+
+import React from 'react'
+import { connect } from 'dva'
+import { Router, Route, Switch } from 'dva/router'
+
+import App from './index'
+import GenericChat from '../Chat/Generic'
+import VideoChat from '../Chat/Video'
+import ContactDetail from '../ContactDetail'
+import createWebsocket from '../../websocket'
+
+
+const mapStateToProps = state => {
+  const { isLoggedIn } = state.wechat
+  return {
+    isLoggedIn
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    dispatch
+  }
+}
+
+
+@connect(mapStateToProps, mapDispatchToProps)
+export default class Component extends React.Component {
+  constructor(props, ctx) {
+    super(props, ctx)
+    const { isLoggedIn, dispatch } = props
+    this.onMessage = this.onMessage.bind(this)
+    if (isLoggedIn && !window.websocket) {
+      const o = createWebsocket(dispatch)
+      const { websocket, addMessageHandler, removeMessageHandler } = o
+      window.websocket = websocket
+      window.addMessageHandler = addMessageHandler
+      window.removeMessageHandler = removeMessageHandler
+    }
+  }componentDidMount() {
+    const { isLoggedIn } = this.props
+    if (isLoggedIn) {
+      window.addMessageHandler(this.onMessage)
+    }
+  }
+
+  componentWillUnmount() {
+    const { isLoggedIn } = this.props
+    if (isLoggedIn) {
+      window.removeMessageHandler(this.onMessage)
+    }
+  }
+
+  onMessage(msg) {
+    const { history } = this.props
+    const { payload } = msg
+    const { from } = payload
+    switch(msg.type) {
+      case 'video-offer': // 联系人发起视频聊天
+        history.push(`/chat/video/${from}`)
+        break
+      default:
+        break
+    }
+  }
+
+  render() {
+    const node = (<Router {...this.props}>
+      <Switch>
+        <Route path="/chat/video/:phone" component={VideoChat} />
+        <Route path="/chat/generic/:phone" component={GenericChat} />
+        <Route path="/contact/:phone([\d_]+)" component={ContactDetail} />
+        <Route path="/:tab(contact|conversation|discovery|my)?" component={App} />
+      </Switch>
+    </Router>)
+    return node
+  }
+}
+
--- src/routes/App/index.jsx
+++ src/routes/App/index.jsx
@@ -1,12 +1,7 @@
 import React from 'react'
 import { connect } from 'dva'
-import { Router, Route, Switch } from 'dva/router'
-import App from '../../containers/App'
+import App from '../../containers/App/route'
 import Login from '../../containers/Login'
-import GenericChat from '../../containers/Chat/Generic'
-import VideoChat from '../../containers/Chat/Video'
-import ContactDetail from '../../containers/ContactDetail'
-import createWebsocket from '../../websocket'
 
 const mapStateToProps = state => {
   const { isLoggedIn } = state.wechat
@@ -23,52 +18,11 @@ const mapDispatchToProps = dispatch => {
 
 @connect(mapStateToProps, mapDispatchToProps)
 class AppRouter extends React.Component {
-  constructor(props, ctx) {
-    super(props, ctx)
-    const { isLoggedIn, dispatch } = props
-    this.onMessage = this.onMessage.bind(this)
-    if (isLoggedIn && !window.websocket) {
-      const o = createWebsocket(dispatch)
-      const { websocket, addMessageHandler, removeMessageHandler } = o
-      window.websocket = websocket
-      window.addMessageHandler = addMessageHandler
-      window.removeMessageHandler = removeMessageHandler
-    }
-  }
-
-  componentDidMount() {
-    window.addMessageHandler(this.onMessage)
-  }
-
-  componentWillUnmount() {
-    window.removeMessageHandler(this.onMessage)
-  }
-
-  onMessage(msg) {
-    const { history } = this.props
-    const { payload } = msg
-    const { from } = payload
-    switch(msg.type) {
-      case 'video-offer': // 联系人发起视频聊天
-        history.push(`/chat/video/${from}`)
-        break
-      default:
-        break
-    }
-  }
-
   render() {
     const { isLoggedIn } = this.props
     let node
     if (isLoggedIn) {
-      node = (<Router {...this.props}>
-        <Switch>
-          <Route path="/chat/video/:phone" component={VideoChat} />
-          <Route path="/chat/generic/:phone" component={GenericChat} />
-          <Route path="/contact/:phone([\d_]+)" component={ContactDetail} />
-          <Route path="/:tab(contact|conversation|discovery|my)?" component={App} />
-        </Switch>
-      </Router>)
+      node = <App {...this.props} />
     } else {
       node = <Login {...this.props} />
     }
