a75834918359c3f6012a0a12cdce249f5ca84408
兴斋 <xingzhai.xxw@alibaba-inc.com>
2018-07-16 17:32:51 +0800

    config.

--- src/components/Chat/Video.jsx
+++ src/components/Chat/Video.jsx
@@ -175,10 +175,10 @@ export default class Component extends React.Component {
           urls: [
             "turn:www.gismall.com:3478"
           ]
+        },
+        {
+          url: 'stun:64.233.188.127:19302'
         }
-        // {
-        //   url: 'stun:64.233.188.127:19302'
-        // }
       ]
     })
   
